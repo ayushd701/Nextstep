b906137fe437fcb62b1facb492af3003d53934b0
ayushd701 <dixitayush0107@gmail.com>
2025-09-23 12:18:33 +0530

    Created few apis.

---
+++ actions/user.js
@@ -0,0 +1,79 @@
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function updateUser(data) {
+  const { userId } = auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+  if (!user) throw new Error("User not found");
+
+  try {
+    const result = await db.$transaction(
+      async (tx) => {
+        let industryInsight = await tx.industryInsight.findUnique({
+          where: {
+            industry: data.industry,
+          },
+        });
+        if (!industryInsight) {
+          industryInsight = await tx.industryInsight.create({
+            data: {
+              industry: data.industry,
+              salaryRanges: [],
+              growthRate: 0,
+              demandLevel: "MEDIUM",
+              topSkills: [],
+              marketOutlook: "NEUTRAL",
+              keyTrends: [],
+              recommendedSkills: [],
+              nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            },
+          });
+        }
+        const updatedUser = await tx.user.update({
+          where: {
+            clerkUserId: userId,
+          },
+          data: {
+            industry: data.industry,
+            experience: data.experience,
+            bio: data.bio,
+            skills: data.skills,
+          },
+        });
+        return { updatedUser, industryInsight };
+      },
+      {
+        timeout: 10000,
+      }
+    );
+    return result.user;
+  } catch (error) {
+    console.error("Error updating user and industry:", error.message);
+    throw new Error("Failed to update profile");
+  }
+}
+
+export async function getUserOnboardingStatus() {
+  const { userId } = auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  try {
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+      select: { industry: true },
+    });
+
+    if (!user) throw new Error("User not found");
+
+    return { isOnboarded: Boolean(user.industry) };
+  } catch (error) {
+    console.error("Error checking onboarding status:", error);
+    throw new Error("Failed to check onboarding status");
+  }
+}
