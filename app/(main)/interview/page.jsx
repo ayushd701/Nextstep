711b3889cb82cb2cf5d55bc01ab2d386e6303ffe
ayushd701 <dixitayush0107@gmail.com>
2025-09-26 20:13:03 +0530

    Interview page completed.

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { getAssessments } from "@/actions/interview";
+import React from "react";
+import StatsCards from "./_components/stats_cards"
+import PerformanceChart from "./_components/performance_chart"
+import QuizList from "./_components/quiz_list"
 
-const Interviewpage = () => {
+
+const Interviewpage = async () => {
+  const assessments = await getAssessments();
+
   return (
     <div>
-      Interviewpage
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Interviewpage
+export default Interviewpage;
