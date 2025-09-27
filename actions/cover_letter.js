3716fd36f23564695a5d4c631813a9caddc77f20
ayushd701 <dixitayush0107@gmail.com>
2025-09-28 00:31:47 +0530

    Api added.

--- actions/cover_letter.js
+++ actions/cover_letter.js
@@ -86,6 +86,24 @@ export async function getCoverLetters() {
   });
 }
 
+export async function getCoverLetter(id) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  return await db.coverLetter.findUnique({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+}
+
 export async function deleteCoverLetter(id) {
   const { userId } = await auth();
   if (!userId) throw new Error("Unauthorized");
